ac8fe3a69517b645d5dcbc49eada3158e7416bb7
Charlie <122215411+Eucalipto-Charlie@users.noreply.github.com>
2025-12-28 09:12:12 +0800

    Update executor.rs

--- src/execution/executor.rs
+++ src/execution/executor.rs
@@ -1,11 +1,11 @@
 use crate::types::{intent::TradeIntent, event::Event};
 
-/// Executor 抽象
-/// 负责接收策略意图，并输出链上已确认事件
+//负责接收策略意图，并输出链上已确认事件
 pub trait Executor {
-    /// 提交交易 Intent 到执行层（可能是 mock 或真实链）
+    //提交交易 Intent 到执行层
     fn submit(&mut self, intent: TradeIntent);
 
-    /// 从执行层获取链上事件（已确认成交等）
+    //从执行层获取链上事件
     fn poll_events(&mut self) -> Vec<Event>;
 }
+
