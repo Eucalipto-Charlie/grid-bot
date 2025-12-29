dbd00d64a4079aef4b5fb5e15d0ff8f89a5aee69
Charlie <122215411+Eucalipto-Charlie@users.noreply.github.com>
2025-12-29 09:17:02 +0800

    Add files via upload

--- src/engine/event_loop.rs
+++ src/engine/event_loop.rs
@@ -3,32 +3,42 @@ use crate::{
     execution::executor::Executor,
     strategy::state_machine::GridStateMachine,
     types::event::Event,
+    persistence::trade_store::TradeStore,
 };
 
-pub struct Engine<F, E> {
+pub struct Engine<F, E, S> {
     feed: F,
     detector: GridCrossDetector,
     strategy: GridStateMachine,
     executor: E,
+    store: S,
 }
 
-impl<F, E> Engine<F, E>
+impl<F, E, S> Engine<F, E, S>
 where
     F: PriceFeed,
     E: Executor,
+    S: TradeStore,
 {
     pub fn new(
         feed: F,
         detector: GridCrossDetector,
         strategy: GridStateMachine,
         executor: E,
+        store: S,
     ) -> Self {
         Self {
             feed,
             detector,
             strategy,
             executor,
+            store,
         }
+    }
+
+    //scenario 调试使用
+    pub fn trade_store(&self) -> &S {
+        &self.store
     }
 
     pub fn debug_snapshot(&self) {
@@ -45,27 +55,35 @@ where
     pub fn run(&mut self) {
         let mut events = Vec::new();
 
-        // 1. 拉价格 → PriceTick
+        // 1. Price
         if let Some(price) = self.feed.poll_price() {
             events.push(Event::PriceTick { price });
 
-            // 2. Detector 基于 price 生成 PriceCrossed
             let crossed = self.detector.on_price(price);
             for idx in crossed {
                 events.push(Event::PriceCrossed { grid_index: idx });
             }
         }
 
-        // 3. Executor 回执
+        // 2. Executor events
         events.extend(self.executor.poll_events());
 
-        // 4. 统一派发
+        // 3. Dispatch
         for event in events {
+            let prev_result_len = self.strategy.trade_results.len();
+
             let intents = self.strategy.handle_event(event);
+
+            // 持久化 TradeIntent
             for intent in intents {
+                self.store.record_intent(&intent);
                 self.executor.submit(intent);
+            }
+
+            let new_results = &self.strategy.trade_results[prev_result_len..];
+            for result in new_results {
+                self.store.record_result(result);
             }
         }
     }
-
 }
